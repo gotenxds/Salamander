7865e4f4133c1cd26053107bb2a7ac440ef0ef10
gotenxds <johndoe@example.com>
2016-06-01 04:23:55 +0300

    ExtractedMethod.

--- src/ship/ship.ts
+++ src/ship/ship.ts
@@ -18,7 +18,7 @@ import MovementSystem from "./systems/movementSystems";
 import WeaponsSystem from "./systems/weaponsSystem";
 
 export default class Ship extends Group {
-    private movementSystem: MovementSystem;
+    private movementSystem:MovementSystem;
     private weaponsSystem:WeaponsSystem;
     private sprite:Sprite;
     private sparkSprite:Sprite;
@@ -32,18 +32,18 @@ export default class Ship extends Group {
         super(game);
 
         this.invisibilityTimer = game.time.create(false);
-        this.invisibilityTimer.loop(3500, () => {
-            this.invisibilityTween.pause();
-            this.alpha = 1;
-            this.invisibilityTimer.stop(false);
-            console.log('stop');
-        });
+        this.invisibilityTimer.loop(3500, () => this.invisivilityPeriodOver());
         this.spawnEndPoint = {x: game.width / 3, y: game.world.centerY};
         this.explosion = createBlue(game);
         this.initializeSprites();
 
-        this.movementSystem = new MovementSystem(game, this, this.sprite, {up:Keyboard.W,down:Keyboard.S,right:Keyboard.D,left:Keyboard.A});
-        this.weaponsSystem = new WeaponsSystem(game, this, this.sparkSprite, {fire:Keyboard.SPACEBAR});
+        this.weaponsSystem = new WeaponsSystem(game, this, this.sparkSprite, {fire: Keyboard.SPACEBAR});
+        this.movementSystem = new MovementSystem(game, this, this.sprite, {
+            up: Keyboard.W,
+            down: Keyboard.S,
+            right: Keyboard.D,
+            left: Keyboard.A
+        });
     }
 
     spawn():void {
@@ -76,26 +76,33 @@ export default class Ship extends Group {
         }
     }
 
-    private resetToMiddleLeft(){
+    private resetToMiddleLeft() :void{
         this.position.set(0, this.game.world.centerY);
     }
 
-    private respawn() :void{
+    private invisivilityPeriodOver():void {
+        this.invisibilityTween.pause();
+        this.alpha = 1;
+        this.invisibilityTimer.stop(false);
+        console.log('stop');
+    }
+
+    private respawn():void {
         this.explosion.play(this.sprite.body);
         this.spawn();
     };
 
-    private checkCollisions() :void{
+    private checkCollisions():void {
         this.game.world.forEachAlive((child) => {
             if (child.key && child.key.startsWith('monsters') && this.game.physics.arcade.collide(this, child)) {
-                if(this.invisibilityTween.isPaused || !this.invisibilityTween.isRunning){
+                if (this.invisibilityTween.isPaused || !this.invisibilityTween.isRunning) {
                     this.sprite.kill();
                 }
             }
         }, this);
     };
 
-    private yetToReachSpawnPoint() :boolean{
+    private yetToReachSpawnPoint():boolean {
         return this.x <= this.spawnEndPoint.x && this.y <= this.spawnEndPoint.y;
     };
 
@@ -105,14 +112,14 @@ export default class Ship extends Group {
         this.initializeTrail();
     };
 
-    private initializeShipSprite() :void{
+    private initializeShipSprite():void {
         this.sprite = this.game.add.sprite(0, 0, 'ship', 'start.png', this);
         this.game.physics.arcade.enable(this.sprite);
         this.sprite.anchor.setTo(.5);
         (<Phaser.Physics.Arcade.Body> this.sprite.body).collideWorldBounds = true;
     };
 
-    private initializeSparkSprite() :void{
+    private initializeSparkSprite():void {
         this.sparkSprite = this.game.add.sprite(60, 6, 'ship.weapons');
         this.sprite.addChild(this.sparkSprite);
         this.sparkSprite.anchor.set(.5, .5);
@@ -122,7 +129,7 @@ export default class Ship extends Group {
         this.invisibilityTween = this.game.add.tween(this).to({alpha: .3}, 200, Phaser.Easing.Linear.None, false, 0, Infinity, true);
     };
 
-    private initializeTrail() :void{
+    private initializeTrail():void {
         let trail = this.game.add.sprite(-38, 8, 'ship.trail');
         trail.scale.set(.7);
         trail.anchor.set(1, .5);
