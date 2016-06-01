e8c7f6198f6a3e86457e3d68010c308855775a27
gotenxds <johndoe@example.com>
2016-06-01 23:35:32 +0300

    Added a Text as image gui component.

---
+++ src/gui/TextAsImage.ts
@@ -0,0 +1,48 @@
+import Group = Phaser.Group;
+import Game = Phaser.Game;
+import Sprite = Phaser.Sprite;
+export default class TextAsImage extends Group {
+    private tag:string;
+    private maxChars:number;
+    private height:number;
+    private text:string;
+    private dirty:boolean = true;
+
+    constructor(game:Game, tag:string, x:number = 0, y:number = 0, text:string = '', maxChars:number = Infinity, height:number = -1) {
+        super(game);
+
+        this.tag = tag;
+        this.maxChars = maxChars;
+        this.text = text;
+        this.x = x;
+        this.y = y;
+
+        if (height === -1) {
+            this.height = this.game.cache.getImage(tag).height;
+        }
+    }
+
+    setText(text:string):void {
+        this.dirty = this.text !== text;
+        this.text = text;
+        this.update();
+    }
+
+    update():void {
+        if (this.dirty) {
+            this.dirty = false;
+
+            this.forEachAlive(letter => letter.kill(), this);
+            let x = 0;
+            let y = 0;
+            let charSprite:Sprite;
+
+            for (let char of this.text) {
+                charSprite = this.getFirstDead(true, x, y, this.tag, char);
+
+                charSprite.revive();
+                x += charSprite.width;
+            }
+        }
+    }
+}
